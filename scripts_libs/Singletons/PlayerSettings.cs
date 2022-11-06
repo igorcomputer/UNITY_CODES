8a37a399d426206022d3f1e53efa51f308ba30ef
igorcomputer <igorcomputer@mail.ru>
2022-11-06 21:39:16 +0300

    Add PlayerSettings singleton

---
+++ scripts_libs/Singletons/PlayerSettings.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerSettings : MonoBehaviour {
+
+    public string Name;
+    public int Difficulty;
+
+    public static PlayerSettings Instance;
+
+    private void Awake() {
+        if (Instance == null) {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        } else {
+            Destroy(gameObject);
+        }
+    }
+
+    public void SetName(string name) {
+        Name = name;
+    }
+
+    public void SetDifficulty(int value) {
+        Difficulty = value + 1;
+    }
+}
